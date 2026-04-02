b39bf2f6b418506c876987f0288a8d30a02a0ca6
fetsjr <fetsjr@users.noreply.github.com>
2026-04-02 00:21:21 -0600

    Enhance precompiler regex and use realpath

--- src/TablerServiceProvider.php
+++ src/TablerServiceProvider.php
@@ -9,17 +9,20 @@ class TablerServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        // 1. Cargamos las vistas del paquete
-        $this->loadViewsFrom(__DIR__.'/../stubs/resources/views/tabler', 'tabler');
+        $path = realpath(__DIR__.'/../stubs/resources/views/tabler');
 
-        // 2. Registramos el path de componentes anónimos
-        Blade::anonymousComponentPath(__DIR__.'/../stubs/resources/views/tabler', 'tabler');
+        if ($path) {
+            // 1. Cargar vistas tradicionales
+            $this->loadViewsFrom($path, 'tabler');
 
-        // 3. REGISTRO DEL PRE-COMPILADOR (Estilo Flux)
-        // Esto traduce <tabler:xxx> a <x-tabler::xxx> antes de que Laravel lo procese
+            // 2. Registrar componentes anónimos
+            Blade::anonymousComponentPath($path, 'tabler');
+        }
+
+        // 3. Pre-compilador estilo Flux
         Blade::precompiler(function ($value) {
-            return preg_replace_callback('/<\/?tabler:([a-z0-9\-\.\:]+)/', function ($matches) {
-                return str_replace('tabler:', 'x-tabler::', $matches[0]);
+            return preg_replace_callback('/(<\/?\s*)tabler:([a-z0-9\-\.]+)/i', function ($matches) {
+                return $matches[1] . 'x-tabler::' . $matches[2];
             }, $value);
         });
     }
