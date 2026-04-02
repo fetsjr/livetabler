3270cd58b1b17e139a4ccfdd41633da362cc55d0
fetsjr <fetsjr@users.noreply.github.com>
2026-04-02 00:26:59 -0600

    Implement advanced Blade precompiler (Flux style) for tabler: support

--- src/TablerServiceProvider.php
+++ src/TablerServiceProvider.php
@@ -4,29 +4,39 @@ namespace Tabler;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\View\Compilers\BladeCompiler;
 
 class TablerServiceProvider extends ServiceProvider
 {
+    /**
+     * Boot the service provider.
+     */
     public function boot(): void
     {
         $path = realpath(__DIR__.'/../stubs/resources/views/tabler');
 
         if ($path) {
-            // 1. Cargar vistas tradicionales
+            // 1. Cargamos las vistas para acceder a ellas vía tabler::
             $this->loadViewsFrom($path, 'tabler');
 
-            // 2. Registrar componentes anónimos
+            // 2. Registramos los componentes anónimos bajo el namespace 'tabler'
             Blade::anonymousComponentPath($path, 'tabler');
         }
 
-        // 3. Pre-compilador estilo Flux
-        Blade::precompiler(function ($value) {
-            return preg_replace_callback('/(<\/?\s*)tabler:([a-z0-9\-\.]+)/i', function ($matches) {
-                return $matches[1] . 'x-tabler::' . $matches[2];
-            }, $value);
+        // 3. REGISTRO DEL PRE-COMPILADOR (Inspirado en la lógica de Flux)
+        // Esto traduce <tabler:...> a <x-tabler::...> antes de que Laravel lo procese
+        $this->app->afterResolving('blade.compiler', function (BladeCompiler $blade) {
+            $blade->precompiler(function ($string) {
+                return preg_replace_callback('/<\/?tabler:([a-z0-9\-\.]+)/i', function ($matches) {
+                    return str_replace('tabler:', 'x-tabler::', $matches[0]);
+                }, $string);
+            });
         });
     }
 
+    /**
+     * Register the service provider.
+     */
     public function register(): void
     {
         //
