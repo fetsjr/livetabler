8b21bb12292cbabdddd13a18b5479de4882437e7
fetsjr <fetsjr@users.noreply.github.com>
2026-04-02 00:09:24 -0600

    Fix ServiceProvider to support <tabler:> syntax

--- src/TablerServiceProvider.php
+++ src/TablerServiceProvider.php
@@ -7,22 +7,34 @@ use Illuminate\Support\Facades\Blade;
 
 class TablerServiceProvider extends ServiceProvider
 {
-    public function register(): void
-    {
-        //
-    }
-
+    /**
+     * Boot the service provider.
+     */
     public function boot(): void
     {
+        // 1. Cargamos el namespace de vistas para usar @include('tabler::...') si fuera necesario
+        if (is_dir(__DIR__.'/../stubs/resources/views/tabler')) {
+            $this->loadViewsFrom(__DIR__.'/../stubs/resources/views/tabler', 'tabler');
+        }
+
+        // 2. Registramos los componentes bajo el namespace 'tabler'
         $this->bootComponentPath();
     }
 
     /**
-     * Register Blade anonymous components under the 'tabler' namespace.
-     * This allows using <tabler:avatar>, <tabler:button>, etc.
+     * Registrar componentes anónimos bajo el namespace 'tabler'.
      */
     public function bootComponentPath(): void
     {
+        // Este es el método estándar de Laravel para <x-tabler::...>
         Blade::anonymousComponentPath(__DIR__.'/../stubs/resources/views/tabler', 'tabler');
+        
+        // IMPORTANTE: Esto es lo que permite usar los dos puntos <tabler:accordion>
+        Blade::componentNamespace('Tabler\\stubs\\resources\\views\\tabler', 'tabler');
+    }
+
+    public function register(): void
+    {
+        //
     }
 }
