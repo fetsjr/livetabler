611f9bd48830cee1092e6f9caa744990b49e57f2
fetsjr <fetsjr@users.noreply.github.com>
2026-04-03 01:00:14 -0600

    fix: apply defensive coding to Icon component and resolve Undefined Variable size exception

--- stubs/resources/views/tabler/icon/index.blade.php
+++ stubs/resources/views/tabler/icon/index.blade.php
@@ -1,2 +1,9 @@
-<i {{ $attributes->merge(['class' => "icon ti ti-{$name} " . ($class ?? '')]) }} 
+@php
+    $name = $name ?? 'help';
+    $size = $size ?? 18;
+    $stroke = $stroke ?? 2;
+    $class = $class ?? '';
+@endphp
+
+<i {{ $attributes->merge(['class' => "icon ti ti-{$name} " . $class]) }} 
    style="font-size: {{ $size }}px; stroke-width: {{ $stroke }};"></i>
