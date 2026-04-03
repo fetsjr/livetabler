5d3c15c83a1fc683dc49569d5b78e38bc8b5fa89
fetsjr <fetsjr@users.noreply.github.com>
2026-04-02 21:27:29 -0600

    Refactor aside, timeline, tooltip, and skeleton components to native Tabler UI

--- stubs/resources/views/tabler/aside.blade.php
+++ stubs/resources/views/tabler/aside.blade.php
@@ -2,6 +2,8 @@
     'sticky' => false,
 ])
 
-<aside {{ $attributes->class(['w-64 flex-shrink-0', $sticky ? 'sticky-top overflow-y-auto max-vh-100' : '']) }}>
-    {{ $slot }}
+<aside {{ $attributes->class(['navbar navbar-vertical navbar-expand-lg d-flex flex-column h-100', $sticky ? 'sticky-top' : '']) }}>
+    <div class="container-fluid overflow-y-auto">
+        {{ $slot }}
+    </div>
 </aside>
